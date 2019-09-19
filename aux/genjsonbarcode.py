9b29e1ef689019c4a2b63a8a442785fd3817ff5d
eyangch <eyangch@gmail.com>
2019-09-19 16:20:43 -0500

    change food name

--- aux/genjsonbarcode.py
+++ aux/genjsonbarcode.py
@@ -4,7 +4,7 @@ oFile = open("datab.json", "w")
 
 output = json.dumps(
 {
-    "074410490577":"seaweed, salted"
+    "074410490577":"seaweed, roasted"
 }, sort_keys = True, indent = 4)
 
 print(output)
